5238bfc7b6d9abc644f369475adbfeaa59ca05f0
francob92 <francobriano67@gmail.com>
2021-08-18 22:20:01 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Personaje v-for="(personaje, index) in personajes" :key="index" :personaje="personaje" />
+  </div>
+</template>
+
+<script>
+import Personaje from './Personaje.vue';
+export default {
+  name: "App",
+  components: { Personaje },
+  data: () => ({
+    personajes: [
+      {
+        nombre: "Rick Sanchez",
+        src: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      },
+      {
+        nombre: "Morty Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+body {
+  background-image: url("../public/background.png");
+}
+</style>
---
+++ src/Personaje.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img :src="personaje.src" alt="personaje">
+    <p>{{personaje.nombre}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Personaje",
+  
+  props: { personaje: { type: Object } },
+};
+</script>
+
+<style>
+div {
+  color: green;
+}
+p {
+  display: inline-block;
+}
+img {
+  border-radius: 50%;
+}
+</style>
